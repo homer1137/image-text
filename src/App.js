bdda4a7b5a7def4f82064a46d6ed96507813f162
Romango <homer37@yandex.ru>
2022-12-10 17:02:27 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import  {useState, useEffect} from 'react'
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [link, setLink] = useState('')
+  const [loader, setLoader] = useState(false);
+  const [eror, setError] = useState('')
+
+  useEffect(()=>{
+    link&&setError('')
+  }, [link])
+  
+  const onSubmit = async(e)=>{
+    e.preventDefault();
+    if(link){
+      setLoader(true)
+      try {
+        const res = await axios.get(
+          'http://localhost:5000/screenshot',
+          {
+            params: {
+              url: link
+            }
+          }
+        );
+        console.log('response', res)
+      } catch (error) {
+        setError(error.message)
+        console.log(error)
+      }
+      setLoader(false)
+      setLink('')
+    }else{
+      setError('Поле ссылка не может быть пустым')
+    }
+   
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+    
+        
+        <form onSubmit={onSubmit} className='form'>
+        {loader&&<div>Идет извлечение текста из изображения...</div>}
+        {eror&&<div>{eror}</div>}
+          <label>Укажите ссылку на изображение</label>
+          <input type='text' value={link} onChange={(e)=>setLink(e.target.value)} placeholder={'https://ya.ru/'}/>
+          <button type='onSubmit'>Извлечь текст</button>
+        </form>
     </div>
   );
 }
