8979666d1ccd0910d12e6fe3ac7b84053ec742d7
Romango <homer37@yandex.ru>
2022-12-16 05:48:24 +0400

    добавил кнопки

--- src/App.js
+++ src/App.js
@@ -1,55 +1,82 @@
-import  {useState, useEffect} from 'react'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [link, setLink] = useState('')
+  const [link, setLink] = useState("");
   const [loader, setLoader] = useState(false);
-  const [eror, setError] = useState('')
+  const [eror, setError] = useState("");
 
-  useEffect(()=>{
-    link&&setError('')
-  }, [link])
-  
-  const onSubmit = async(e)=>{
+  useEffect(() => {
+    link && setError("");
+  }, [link]);
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if(link){
-      setLoader(true)
+    if (link) {
+      setLoader(true);
       try {
-        const res = await axios.get(
-          'http://localhost:5000/screenshot',
-          {
-            params: {
-              url: link
-            }
-          }
-        );
-        console.log('response', res)
+        const res = await axios.get("http://localhost:5000/screenshot", {
+          params: {
+            url: link,
+          },
+        });
+        console.log("response", res);
       } catch (error) {
-        setError(error.message)
-        console.log(error)
+        setError(error.message);
+        console.log(error);
       }
-      setLoader(false)
-      setLink('')
-    }else{
-      setError('Поле ссылка не может быть пустым')
+      setLoader(false);
+      setLink("");
+    } else {
+      setError("Поле ссылка не может быть пустым");
     }
+  };
+
+  const onSubmit2 = async (e, number) => {
+    e.preventDefault();
+    
+      setLoader(true);
+      try {
+        const res = await axios.get(`http://localhost:5000/screenshot${number}`);
+        console.log("response", res);
+      } catch (error) {
+        setError(error.message);
+        console.log(error);
+      }
+      setLoader(false);
+      setLink("");
    
-   
-  }
+  };
 
   return (
-    <div className='wrapper'>
-    
+    <div className="wrapper">
+    {loader && <div>Идет извлечение текста из изображения...</div>}
+        {eror && <div>{eror}</div>}
+      <form onSubmit={onSubmit} className="form">
         
-        <form onSubmit={onSubmit} className='form'>
-        {loader&&<div>Идет извлечение текста из изображения...</div>}
-        {eror&&<div>{eror}</div>}
-          <label>Укажите ссылку на изображение</label>
-          <input type='text' value={link} onChange={(e)=>setLink(e.target.value)} placeholder={'https://ya.ru/'}/>
-          <button type='onSubmit'>Извлечь текст</button>
-        </form>
+        <label>Скрин окна браузера по сылке</label>
+        <input
+          type="text"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          placeholder={"https://ya.ru/"}
+        />
+        <button type="onSubmit">Извлечь текст</button>
+      </form>
+      <form onSubmit={(e)=>onSubmit2(e, 2)} className="form">
+        
+        <label>С библиотекой - скрин экрана (linux, windows)</label>
+    
+        <button type="onSubmit">Извлечь текст</button>
+      </form>
+      <form onSubmit={(e)=>onSubmit2(e, 3)} className="form">
+        
+        <label>скрин экрана (linux)</label>
+    
+        <button type="onSubmit">Извлечь текст</button>
+      </form>
     </div>
   );
 }
